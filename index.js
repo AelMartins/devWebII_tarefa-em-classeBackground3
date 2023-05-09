aa98396b22e20a8e0fd7da246f54215f1ccbf7c7
AelMartins <oneapcwork@gmail.com>
2023-05-09 20:23:05 -0300

    configured CORS middleware

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ app.use(express.json());
 app.listen(8080, () => {
     console.log("O SERVIDOR ESTA ATIVO NA PORTA 8080");
 })
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+      return res.status(200).json({});
+    }
+    next();
+  });
