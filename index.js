e86c5b1d6d00637da283c84efd55e58b7ca636a2
AelMartins <oneapcwork@gmail.com>
2023-05-11 15:40:46 -0300

    created POST add new user

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 //   app.get("/") -> "Home Page"
 
   app.get('/', (req, res) => {
-    res.send('HOME PAGE');
+    res.send(`HOME PAGE`);
   });
 
 //   app.post("/login") -> Chamada login
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
     const user = result.recordset[0];
 
     if (!user || !await bcrypt.compare(password, user.password)) {
-      return res.status(401).send('INFORMAÇÕES INCORRETAS!');
+      return res.status(401).send(`INFORMAÇÕES INCORRETAS!`);
     }
 
     const token = jwt.sign({ userId: user.id }, dbConfig.tchave);
@@ -55,7 +55,7 @@ app.get('/users', async (req, res) => {
       const decodedToken = jwt.verify(token, dbConfig.tchave);
       req.userData = { userId: decodedToken.userId };
     } catch (error) {
-      return res.status(401).send('TOKEN INVÁLIDO!');
+      return res.status(401).send(`TOKEN INVÁLIDO!`);
     }
 
     const pool = await sql.connect(dbConfig);
@@ -68,6 +68,39 @@ app.get('/users', async (req, res) => {
 
 //   app.post("/users") -> Criar um usuário
 
+app.post('/users', async (req, res) => {
+    let pool;
+  
+    try {
+      pool = await mssql.connect(dbConfig);
+      const result = await pool.request()
+        .input('name', mssql.VarChar, req.body.name)
+        .query('SELECT COUNT(*) AS count FROM users WHERE name = @name');
+  
+      const countUsers = result.recordset[0].count;
+  
+      if (countUsers > 0) {
+        return res.status(400).json({ error: `NOME DE USUÁRIO JÁ REGISTRADO` });
+      }
+
+      const password = await bcrypt.hash(req.body.password, dbConfig.cryptHash);
+  
+      const insertUser = await pool.request()
+        .input('name', mssql.VarChar, req.body.name)
+        .input('password', mssql.VarChar, password)
+        .query('INSERT INTO Users (name, password) VALUES (@name, @password)');
+  
+      const newUser = insertUser.recordset;
+  
+      res.status(200).send(`USUÁRIO " ${newUser} " ADICIONADO!`);
+    } catch (error) {
+      res.status(500).json({ error: `ERRO AO CRIAR USUÁRIO: ${error.message}` });
+    } finally {
+        if (pool){
+            pool.close();
+        }
+    }
+  });
 
 //   app.put("/users/:id") -> Atualizar um usuário
 
